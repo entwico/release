e348e936ce52577e74514e431fe20679c26aa1da
Simon Bobrov <smnbbrv@users.noreply.github.com>
2026-04-17 09:17:15 +0200

    fix: switch to esm

--- release.js
+++ release.js
@@ -1,48 +1,47 @@
 #!/usr/bin/env node
 
-const isClean = require('git-is-clean')
-const findGitRoot = require('find-git-root')
-const path = require('path')
+import isClean from 'git-is-clean';
+import findGitRoot from 'find-git-root';
+import path from 'path';
+import semanticRelease from 'semantic-release';
 
-isClean()
-  .then(clean => {
-    if (!clean) {
-      console.error('The git repository is not clean. Please commit or stash all the changes');
-      process.exit(1);
+const clean = await isClean();
+
+if (!clean) {
+  console.error('The git repository is not clean. Please commit or stash all the changes');
+  process.exit(1);
+}
+
+const root = findGitRoot(process.cwd());
+
+if (!root) {
+  console.error('Could not find git root');
+  process.exit(1);
+}
+
+process.chdir(path.join(root, '..'));
+
+await semanticRelease({
+  ci: false,
+  branches: [
+    "master",
+    "main",
+  ],
+  verifyConditions: [
+    "@semantic-release/changelog",
+    "@semantic-release/git",
+  ],
+  prepare: [
+    "@semantic-release/changelog",
+    {
+      path: "@semantic-release/git",
+      assets: [
+        "CHANGELOG.md"
+      ],
+      message: "chore: generate CHANGELOG.md for release version ${nextRelease.version}"
     }
-
-    const root = findGitRoot(process.cwd());
-
-    if (!root) {
-      console.error('Could not find git root');
-      process.exit(1);
-    }
-
-    process.chdir(path.join(root, '..'));
-
-    require('semantic-release')({
-      ci: false,
-      branches: [
-        "master",
-        "main",
-      ],
-      verifyConditions: [
-        "@semantic-release/changelog",
-        "@semantic-release/git",
-      ],
-      prepare: [
-        "@semantic-release/changelog",
-        {
-          path: "@semantic-release/git",
-          assets: [
-            "CHANGELOG.md"
-          ],
-          message: "chore: generate CHANGELOG.md for release version ${nextRelease.version}"
-        }
-      ],
-      publish: false,
-      success: false,
-      fail: false,
-    });
-  })
-
+  ],
+  publish: false,
+  success: false,
+  fail: false,
+});
